d373c9a925c2ca617432f8efc9a6e84c9746ed32
NickGrattan <nick@nickgrattan.net>
2018-02-28 15:42:33 +0000

    Initial Version

--- word2vec.py
+++ word2vec.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 """Basic word2vec example."""
 
+# OUTPUT
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
